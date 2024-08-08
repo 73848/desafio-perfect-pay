a247e84480d5620fdab4c787deb7411b383eb36c
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-08-08 12:28:03 -0300

    configuracao de testes para a pagina de edicao de produtos ja cadastrados

---
+++ tests/Feature/Views/EditProductViewTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+# php artisan test --filter=ProductControllerTest
+class EditProductViewTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_crud_products_view(){
+        $product = Product::factory(30)->create();
+        $product = $product->first();
+
+        $response = $this->get("/edit-product/{$product->id}");
+        $response->assertStatus(200);
+        $response->assertViewHas('product', $product);
+        $response->assertViewIs('edit_product');
+
+    }
+
+
+
+}
