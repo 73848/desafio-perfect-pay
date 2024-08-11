60ca26855093a0a0d714ee0a2c5a5043ba23d7e5
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-08-11 13:47:26 -0300

    criacao da funcao banco_de_dados_aplicacao para converter date do db no formato desejado pela aplicacao

--- app/Helpers/helpers_functions.php
+++ app/Helpers/helpers_functions.php
@@ -5,3 +5,10 @@
     $dateFormat = \DateTime::createFromFormat($format, $date_from_app )->format('Y-m-d');
     return $dateFormat;
 };
+
+function banco_de_dados_aplicacao($date_from_db){
+    $format = 'Y-m-d';
+    $date_formated = \DateTime::createFromFormat($format, $date_from_db )->format('d/m/Y');
+    return $date_formated;
+
+}
--- tests/Unit/Helpers/Banco_de_dados_aplicacaoTest.php
+++ tests/Unit/Helpers/Banco_de_dados_aplicacaoTest.php
@@ -19,6 +19,9 @@ class HelpersDBAplication extends TestCase
         
     }
 
+   
+    
+
 
 
 
