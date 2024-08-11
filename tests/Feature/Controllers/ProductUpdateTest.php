db13ea8347f1992f8e7c2a17ccf47234eb415eb0
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-08-11 09:22:03 -0300

    testando update de produtos

--- tests/Feature/Controllers/ProductUpdateTest.php
+++ tests/Feature/Controllers/ProductUpdateTest.php
@@ -9,26 +9,34 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
-# php artisan test --filter=ProductRegistration
-class ProductRegistration extends TestCase
+# php artisan test --filter=ProductUpdate
+class ProductUpdate extends TestCase
 {   
     use RefreshDatabase;
     use WithoutMiddleware;
-    public function test_product_are_set_correctly(){
+    public function test_product_are_update_correctly(){
         $productData = [
             'name'=>'Iphone 7',
             'description'=>'O melhor móvel da atualizade',
             'price' => '800',
         ];
-        
+        $productUpdate = [
+            'name'=>'Iphone 15',
+            'description'=>'Esse sim é o melhor.',
+            'price' => '10000',
+        ];
+
         $product = Product::factory()->create($productData);
+        $product->update($productUpdate);
         
         $response = $this->post('/register',$productData );
-
         $response->assertStatus(302);
-        $this->assertDatabaseHas('products', ['name'=>'Iphone 7']);
-        $this->assertDatabaseHas('products',['description'=>'O melhor móvel da atualizade']);
-        $this->assertDatabaseHas('products', ['price' => '800']);
+
+        $this->assertDatabaseHas('products', ['name'=>'Iphone 15']);
+        $this->assertDatabaseHas('products',['description'=>'Esse sim é o melhor.']);
+        $this->assertDatabaseHas('products', ['price' => '10000']);
+    
+
     }
 
 
