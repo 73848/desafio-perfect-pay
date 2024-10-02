20c515e5d2ab8454878c2b3b6facaba63c87abcb
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-10-01 22:47:44 -0300

    correcao do erro no teste de dashboard, pois estava sendo retornado codigo 302, quando deveria retornar o 200. a solucao foi criar um actingAs para iniciar a sessao do usuario e evitar o redirecionamento

--- tests/Feature/Views/DashboardViewTest.php
+++ tests/Feature/Views/DashboardViewTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Product;
+use App\Models\Users;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,6 +13,10 @@ class DashboardViewTest extends TestCase
 {
     use RefreshDatabase;
     public function test_dashboard_view_product(){
+        
+        $admin = Users::factory()->create();
+        $this->actingAs($admin)->withSession(['role_id'=>'1', 'user_id'=> '7']);
+
         $products = Product::factory(30)->create();
         $product = $products->first();
 
